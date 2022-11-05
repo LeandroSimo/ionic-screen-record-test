1e51cd649019a09da05573eee6607307cb1c9431
LeandriSimo <leo.bt.12@hotmail.com>
2022-11-04 23:00:01 -0300

    Download do arquivo canva

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,134 @@
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 import './ExploreContainer.css';
 
 interface ContainerProps {
   name: string;
 }
 
+var canvas:  HTMLCanvasElement | null;
+var context:CanvasRenderingContext2D | null | undefined;
+var recordBtn: HTMLButtonElement | null
+
+
+let recording = false;
+let mediaRecorder: MediaRecorder;
+let recordedChunks: BlobPart[] | undefined;
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   console.log("Documento: ", document)
+//   canvas = document.querySelector("canvas");
+//   context  = canvas?.getContext("2d");
+//   recordBtn = document.querySelector("button");
+//   init()
+//   draw()
+// });
+
+
+
+setTimeout(() => {
+  canvas = document.querySelector("canvas");
+  context  = canvas?.getContext("2d");
+  recordBtn = document.querySelector("button");
+  init()
+  draw()
+}, 2000)
+
+
+
+function init(){
+recordBtn!.addEventListener("click", () => {
+  console.log('Entrando na função')
+  recording = !recording;
+  if(recording){
+      recordBtn!.textContent = "Stop";
+      const stream = canvas?.captureStream(25);
+      mediaRecorder = new MediaRecorder(stream!, {
+        mimeType: 'video/webm;codecs=vp9',
+        // ignoreMutedMedia: true
+      });
+      recordedChunks = [];
+      mediaRecorder.ondataavailable = e => {
+        if(e.data.size > 0){
+          recordedChunks?.push(e.data);
+        }
+      };
+      mediaRecorder.start();
+    } else {
+      recordBtn!.textContent = "Record"
+      mediaRecorder.stop();
+      setTimeout(() => {
+        const blob = new Blob(recordedChunks,{
+          type: "video/webm"
+        });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+
+        console.log('Salvando a aplicação')
+        a.href = url;
+        a.download = "recording.webm";
+        a.click();
+        
+        // DOWNLOAD DO ARQUIVO
+        Filesystem.writeFile({
+        path: 'media/video.webm',
+        data: a.type,
+        // data: canvas!.toDataURL(),
+        directory: Directory.ExternalStorage,
+        recursive: true,
+        encoding: Encoding.UTF8
+      });
+        document.body.removeChild(a);
+        // URL.revokeObjectURL(url);
+      },0);
+    }
+});
+}
+
+//Canvas animation to rotate colors
+var hue = 0;
+
+function draw(){
+  hue += 1;
+  if(hue > 359){
+    hue = 0;
+  }
+
+  if(context !== undefined){
+  context?.clearRect(0, 0, 50, 50);
+  context!.fillStyle = `hsl(${hue}, 100%, 50%)`;
+  context?.fillRect(0, 0, 50, 50)
+  }
+
+  setTimeout(() => {
+    draw()
+  }, 20)
+  
+  // requestAnimationFrame(draw);
+}
+
+// requestAnimationFrame(draw);
+// draw();
+
+
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+
   return (
     <div className="container">
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a>
+      <br />
+      <canvas height="50" width="50">
+
+
+      </canvas>
+      <br />
+      <button>Record</button>
+      
+      </p>
     </div>
   );
 };
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,74 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
+
+
 const Tab1: React.FC = () => {
+
+// let start = document.getElementById('start'),
+//     stop  = document.getElementById('stop'),
+//     mediaRecorder: MediaRecorder;
+
+// start?.addEventListener('click', async function(){
+//     let stream = await recordScreen();
+//     let mimeType = 'video/webm';
+//     mediaRecorder = createRecorder(stream, mimeType);
+//   let node = document.createElement("p");
+//     node.textContent = "Started recording";
+//     document.body.appendChild(node);
+// })
+
+// stop?.addEventListener('click', function(){
+//     mediaRecorder.stop();
+//     let node = document.createElement("p");
+//     node.textContent = "Stopped recording";
+//     document.body.appendChild(node);
+// })
+
+// async function recordScreen() {
+//     return await navigator.mediaDevices.getDisplayMedia({
+//         audio: false, 
+//         video: true
+//     });
+// }
+
+// function createRecorder (stream: MediaStream, mimeType: string) {
+//   // the stream data is stored in this array
+//   let recordedChunks: Blob[] = []; 
+
+//   const mediaRecorder = new MediaRecorder(stream);
+
+//   mediaRecorder.ondataavailable = function (e) {
+//     if (e.data.size > 0) {
+//       recordedChunks.push(e.data);
+//     }  
+//   };
+//   mediaRecorder.onstop = function () {
+//      saveFile(recordedChunks);
+//      recordedChunks = [];
+//   };
+//   mediaRecorder.start(200); // For every 200ms the stream data will be stored in a separate chunk.
+//   return mediaRecorder;
+// }
+
+// function saveFile(recordedChunks: any[] | undefined){
+
+//    const blob = new Blob(recordedChunks, {
+//       type: 'video/webm'
+//     });
+//     let filename = window.prompt('Enter file name'),
+//         downloadLink = document.createElement('a');
+//     downloadLink.href = URL.createObjectURL(blob);
+//     downloadLink.download = `${filename}.webm`;
+
+//     document.body.appendChild(downloadLink);
+//     downloadLink.click();
+//     URL.revokeObjectURL(blob.type); // clear from memory
+//     document.body.removeChild(downloadLink);
+// }
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,9 +83,31 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <IonButton >
+          <IonLabel>Start</IonLabel>
+        </IonButton>
+        <IonButton >
+          <IonLabel>Stop</IonLabel>
+        </IonButton>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Tab1;
+function useState(arg0: null): [any, any] {
+  throw new Error('Function not implemented.');
+}
+
+function useRef(arg0: null) {
+  throw new Error('Function not implemented.');
+}
+
+function invokeSaveAsDialog(blob: any) {
+  throw new Error('Function not implemented.');
+}
+
+function useEffect(arg0: () => void, arg1: any[]) {
+  throw new Error('Function not implemented.');
+}
+
